4bebcc6ca647e7fa5f4bd978cca829ee128fbc94
delba <delba@users.noreply.github.com>
2015-03-20 15:57:53 -0400

    Index and Embed actions

--- main.go
+++ main.go
@@ -1,4 +1,29 @@
 package main
 
+import (
+	"html/template"
+	"net/http"
+	"path"
+)
+
+func handle(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles(path.Join("views", "index.html"))
+	handle(err)
+
+	err = t.Execute(w, nil)
+}
+
+func Embed(w http.ResponseWriter, r *http.Request) {
+
+}
+
 func main() {
+	http.HandleFunc("/", Index)
+	http.HandleFunc("/embed", Embed)
 }
