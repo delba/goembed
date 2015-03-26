87ab4db616d226c8f54e5ed3f683a1558a3b1c26
delba <delba@users.noreply.github.com>
2015-03-26 16:45:52 -0400

    Extract controller methods in controller package

---
+++ controller/videos.go
@@ -0,0 +1,58 @@
+package controller
+
+import (
+	"encoding/json"
+	"html/template"
+	"net/http"
+	"path"
+	"strconv"
+
+	"github.com/delba/goembed/model"
+)
+
+func handle(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles(path.Join("views", "index.html"))
+	handle(err)
+
+	items, err := model.AllItems()
+	handle(err)
+
+	err = t.Execute(w, items)
+	handle(err)
+}
+
+func Show(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query()["id"][0])
+	handle(err)
+
+	item, err := model.FindItem(id)
+	handle(err)
+
+	data, err := json.Marshal(item)
+	handle(err)
+
+	w.Write(data)
+}
+
+func Create(w http.ResponseWriter, r *http.Request) {
+	url := r.FormValue("url")
+	if url == "" {
+		w.WriteHeader(http.StatusNotAcceptable)
+		w.Write([]byte("Url param can't be blank"))
+		return
+	}
+
+	item, err := model.CreateItem(url)
+	handle(err)
+
+	data, err := json.Marshal(item)
+	handle(err)
+
+	w.Write(data)
+}
--- main.go
+++ main.go
@@ -1,14 +1,10 @@
 package main
 
 import (
-	"encoding/json"
-	"html/template"
 	"net/http"
 	"os"
-	"path"
-	"strconv"
 
-	"github.com/delba/goembed/model"
+	"github.com/delba/goembed/controller"
 )
 
 func handle(err error) {
@@ -17,56 +13,15 @@ func handle(err error) {
 	}
 }
 
-func Index(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles(path.Join("views", "index.html"))
-	handle(err)
-
-	items, err := model.AllItems()
-	handle(err)
-
-	err = t.Execute(w, items)
-	handle(err)
-}
-
-func Show(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.URL.Query()["id"][0])
-	handle(err)
-
-	item, err := model.FindItem(id)
-	handle(err)
-
-	data, err := json.Marshal(item)
-	handle(err)
-
-	w.Write(data)
-}
-
-func Create(w http.ResponseWriter, r *http.Request) {
-	url := r.FormValue("url")
-	if url == "" {
-		w.WriteHeader(http.StatusNotAcceptable)
-		w.Write([]byte("Url param can't be blank"))
-		return
-	}
-
-	item, err := model.CreateItem(url)
-	handle(err)
-
-	data, err := json.Marshal(item)
-	handle(err)
-
-	w.Write(data)
-}
-
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
 
-	http.HandleFunc("/", Index)
-	http.HandleFunc("/items/", Show)
-	http.HandleFunc("/embed", Create)
+	http.HandleFunc("/", controller.Index)
+	http.HandleFunc("/items/", controller.Show)
+	http.HandleFunc("/embed", controller.Create)
 
 	fs := http.FileServer(http.Dir("public"))
 	http.Handle("/public/", http.StripPrefix("/public/", fs))
